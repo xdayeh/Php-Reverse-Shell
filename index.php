2857bdfdba142e25ff9c5914cf49d5cb7e747e56
Mohammad AbuDayeh <Mohammad.Abudayeh@htu.edu.jo>
2020-08-24 13:12:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Git Cyber</title>
+  <meta content="The anti-blackmail team at GitCyber will never fail to express your requests for help in confronting online blackmailers. We have a technical team that can protect you." name="descriptison">
+  <meta content="The anti-blackmail team at GitCyber will never fail to express your requests for help in confronting online blackmailers. We have a technical team that can protect you." name="keywords">
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+  <header id="header" class="fixed-top">
+    <div class="container">
+      <div class="logo float-left">
+        <a href="index.php"><img src="assets/img/logo.png" alt="Git Cyber" class="img-fluid"></a>
+      </div>
+      <nav class="main-nav float-right d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="https://gitcyber.com/forum/">Fourm</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <section id="intro" class="clearfix">
+    <div class="container" data-aos="fade-up">
+      <div class="intro-img" data-aos="zoom-out" data-aos-delay="200">
+        <img src="assets/img/intro-img.svg" alt="" class="img-fluid">
+      </div>
+      <div class="intro-info" data-aos="zoom-in" data-aos-delay="100">
+        <h2>The Next Big Thing In<br><span>CyberSecurity</span><br>Training Is Here</h2>
+        <div>
+          <a href="#about" class="btn-get-started scrollto">Get Started</a>
+          <a href="#services" class="btn-services scrollto">Our Services</a>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <main id="main">
+    <section id="about">
+      <div class="container" data-aos="fade-up">
+        <header class="section-header">
+          <h3>About Us</h3>
+          <p>We offer free hacking services, hacking protection, and explanations in cybersecurity</p>
+        </header>
+        <div class="row about-container">
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <p>
+              A team accredited by many international courts and we provide free services to protect against cyber attacks, methods of prevention from attacks, secrets of security, protection and many services.
+            </p>
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Protection of websites and servers</a></h4>
+              <p class="description">We provide services in hacking operations and methods of protection from cyber attacks, allocating servers and developing free systematic plans.</p>
+            </div>
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <div class="icon"><i class="fa fa-photo"></i></div>
+              <h4 class="title"><a href="">Protection of electronic devices</a></h4>
+              <p class="description">Free services in ways to protect from hacking devices and ways to address any hacking of electronic devices from cyber attacks.</p>
+            </div>
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+              <div class="icon"><i class="fa fa-bar-chart"></i></div>
+              <h4 class="title"><a href="">Latest news of cyber attacks</a></h4>
+              <p class="description">We provide the latest exclusive news in the field of information technology</p>
+            </div>
+          </div>
+
+          <div class="col-lg-6 background order-lg-2" data-aos="zoom-in">
+            <img src="assets/img/about-img.svg" class="img-fluid" alt="">
+          </div>
+        </div>
+        <div class="row about-extra">
+          <div class="col-lg-6" data-aos="fade-right">
+            <img src="assets/img/about-extra-1.svg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-5 pt-lg-0" data-aos="fade-left">
+            <h4>Always remember this protection before penetration</h4>
+            <p>
+              We are available 24 hours to help any exceptional case that anyone from any country is exposed to, and we provide services to address cases related to electronic extortion.
+            </p>
+            <p>
+              All the information provided by any person will be related to the person concerned, and no information will be displayed between the team. The contact is between the person and the security expert only.
+            </p>
+          </div>
+        </div>
+        <div class="row about-extra">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left">
+            <img src="assets/img/about-extra-2.svg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-right">
+            <h4>Certified by the Middle East countries.</h4>
+            <p>
+              Our team holds international certificates in the field of ethical hacking, and their collection is licensed and experts in the field of cybersecurity and accredited by the courts of countries.
+            </p>
+            <p>
+              We try as much as possible to create a safe work environment. We will not ignore any inquiries or services, all of which are free, for the sake of a better future.
+            </p>
+            <p>
+              All topics and software available at GitCyber are under supervision, and all are licensed and free of charge
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="services" class="section-bg">
+      <div class="container" data-aos="fade-up">
+        <header class="section-header">
+          <h3>Services</h3>
+          <p>Laudem latine persequeris id sed, ex fabulas delectus quo. No vel partiendo abhorreant vituperatoribus.</p>
+        </header>
+        <div class="row justify-content-center">
+          <div class="col-md-6 col-lg-5" data-aos="zoom-in" data-aos-delay="100">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
+              <h4 class="title"><a href="">Anti-extortion</a></h4>
+              <p class="description">Professional lawyers, Psychiatrists, Technical experts, Security experts, The GitCyber team deals with cases of blackmail and electronic threats on all different social media (Instagram, Facebook, Twitter, Imo, Line, WhatsApp, Snapchat, YouTube).</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-5" data-aos="zoom-in" data-aos-delay="200">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-bookmarks-outline" style="color: #e9bf06;"></i></div>
+              <h4 class="title"><a href="">training courses</a></h4>
+              <p class="description">Prevention is better than cure. We provide training courses for individuals and companies, especially prominent personalities, commercial families and government agencies, towards the safe use of social media.Technically qualifying the risk.</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-5" data-aos="zoom-in" data-aos-delay="100">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-paper-outline" style="color: #3fcdc7;"></i></div>
+              <h4 class="title"><a href="">Our legal services</a></h4>
+              <p class="description">If you want a lawyer for blackmail cases or the best lawyers to solve cases of blackmail and cyber threat, you are in the right place.Whatever the electronic threat or blackmail coming to you, rest assured.</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-5" data-aos="zoom-in" data-aos-delay="200">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-speedometer-outline" style="color:#41cf2e;"></i></div>
+              <h4 class="title"><a href="">Reputation protection</a></h4>
+              <p class="description">In order to protect reputation on the Internet and manage the risks that some may face in the digital world, especially on social media, there are several systematic and organized strategies, methods and mechanisms.</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-5" data-aos="zoom-in" data-aos-delay="100">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-world-outline" style="color: #d6ff22;"></i></div>
+              <h4 class="title"><a href="">Electronic hacking</a></h4>
+              <p class="description">Our company works in accordance with the laws, regulations and instructions that consider the electronic hacking "hackers" a crime punishable by law. Through us, we will provide you with the technical and legal solutions to properly end your blackmail.</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-5" data-aos="zoom-in" data-aos-delay="200">
+            <div class="box">
+              <div class="icon"><i class="ion-ios-clock-outline" style="color: #4680ff;"></i></div>
+              <h4 class="title"><a href="">Confronting bullying</a></h4>
+              <p class="description">If your child is prone to bullying, there are ways to help him deal with this phenomenon and contribute to mitigating its negative effects on him.Even if your home is safe from bullying at the moment, you should talk to your children about this so they are ready.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section id="why-us">
+      <div class="container" data-aos="fade-up">
+        <header class="section-header">
+          <h3>Why choose us?</h3>
+          <p>There are some fictitious entities that erect in the name of the crime of electronic blackmail in the Middle East, as they are the only strong, effective and effective entities that face such crimes.</p>
+        </header>
+
+        <div class="row row-eq-height justify-content-center">
+          <div class="col-lg-4 mb-4">
+            <div class="card" data-aos="zoom-in" data-aos-delay="100">
+              <i class="fa fa-diamond"></i>
+              <div class="card-body">
+                <h5 class="card-title">Legally licensed</h5>
+                <p class="card-text">Many fictitious entities trying to deceive citizens claim that licensing is useless and is not required for anything.</p>
+                <a href="#" class="readmore">Read more </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+            <div class="card" data-aos="zoom-in" data-aos-delay="200">
+              <i class="fa fa-language"></i>
+              <div class="card-body">
+                <h5 class="card-title">An important educational entity</h5>
+                <p class="card-text">It is not just a specialized security company that does its duty to eradicate the crime of electronic extortion in the Middle East and beyond.</p>
+                <a href="#" class="readmore">Read more </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+            <div class="card" data-aos="zoom-in" data-aos-delay="300">
+              <i class="fa fa-object-group"></i>
+              <div class="card-body">
+                <h5 class="card-title">Professional lawyers</h5>
+                <p class="card-text">Specializes only in such cases, it is a team that believes in specialization and the team tries to help you legally after contacting us directly and starting their legal procedures. </p>
+                <a href="#" class="readmore">Read more </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row counters" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">274</span>
+            <p>Resolve issues</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">421</span>
+            <p>Topic</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">1,364</span>
+            <p>Hours Of Support</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">18</span>
+            <p>Experts</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <section id="team">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h3>Team</h3>
+          <p>We have an expert and trained technical team who is fully aware of all the methods and methods that hackers use to hack accounts, and then our team can technically stop the blackmailer, and provide the necessary protection to our customers.</p>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6" data-aos="zoom-out" data-aos-delay="100">
+            <div class="member">
+              <img src="assets/img/team-1.jpg" class="img-fluid" alt="">
+              <div class="member-info">
+                <div class="member-info-content">
+                  <h4>Mohammad AbuDayeh</h4>
+                  <span>Security expert</span>
+                  <div class="social">
+                    <a href="https://facebook.com/xdayeh"><i class="fa fa-twitter"></i></a>
+                    <a href="https://twitter.com/xdayeh"><i class="fa fa-facebook"></i></a>
+                    <a href="https://jo.linkedin.com/in/abudayeh"><i class="fa fa-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6" data-aos="zoom-out" data-aos-delay="200">
+            <div class="member">
+              <img src="assets/img/team-2.jpg" class="img-fluid" alt="">
+              <div class="member-info">
+                <div class="member-info-content">
+                  <h4>Eslam Quraan</h4>
+                  <span>Security expert</span>
+                  <div class="social">
+                    <a href=""><i class="fa fa-twitter"></i></a>
+                    <a href=""><i class="fa fa-facebook"></i></a>
+                    <a href=""><i class="fa fa-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6" data-aos="zoom-out" data-aos-delay="300">
+            <div class="member">
+              <img src="assets/img/team-3.jpg" class="img-fluid" alt="">
+              <div class="member-info">
+                <div class="member-info-content">
+                  <h4>Omer AlHadba</h4>
+                  <span>Security expert</span>
+                  <div class="social">
+                    <a href=""><i class="fa fa-twitter"></i></a>
+                    <a href=""><i class="fa fa-facebook"></i></a>
+                    <a href=""><i class="fa fa-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6" data-aos="zoom-out" data-aos-delay="400">
+            <div class="member">
+              <img src="assets/img/team-4.jpg" class="img-fluid" alt="">
+              <div class="member-info">
+                <div class="member-info-content">
+                  <h4>Esraa Tirsen</h4>
+                  <span>Professional lawyer</span>
+                  <div class="social">
+                    <a href=""><i class="fa fa-twitter"></i></a>
+                    <a href=""><i class="fa fa-facebook"></i></a>
+                    <a href=""><i class="fa fa-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+  </main>
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="social-links text-center">
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Gitcyber</strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer>
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
